1ca8692ae34840952824ab0dc9e96cd7e27068d9
patriciadunphy <dunphy.pm@gmail.com>
2020-08-20 11:41:45 +0200

    Simple form tutorial in React

--- react-intro/src/App.js
+++ react-intro/src/App.js
@@ -1,26 +1,76 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Greetings from "./Greetings";
+import style from "./style";
+import TextField from './TextField';
+import FirstNameField from './FirstNameField';
+import LastNameField from './LastNameField';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class SimpleForm extends React.Component {
+  state = {
+    firstName: "",
+    firstNameError: "",
+  };
+
+  validateName = name => {
+    const regex = /[A-Za-z]{3,}/;
+
+    return !regex.test(name)
+      ? "The name must contain at least three letters. Numbers and special characters are not allowed."
+      : "";
+  };
+
+  onFirstNameBlur = () => {
+    const { firstName } = this.state;
+
+    const firstNameError = this.validateName( firstName );
+
+    return this.setState({ firstNameError });
+  };
+
+  onFirstNameChange = event =>
+    this.setState({
+      firstName: event.target.value
+    });
+
+    onLastNameBlur = () => {
+      const { lastName } = this.state;
+  
+      const lastNameError = this.validateName(lastName);
+  
+      return this.setState({ lastNameError });
+    };
+
+    onLastNameChange = event =>
+    this.setState({
+      lastName: event.target.value
+    });  
+
+    render() {
+      const { firstNameError, firstName, lastName, lastNameError } = this.state;
+  
+      return (
+        <div style={style.form}>
+          <FirstNameField onChange={this.onFirstNameChange}
+                          onBlur={this.onFirstNameBlur}
+                          error={firstNameError} />
+  
+          <LastNameField onChange={this.onLastNameChange}
+                          onBlur={this.onLastNameBlur}
+                          error={lastNameError} />
+  
+          <Greetings firstName={firstName} lastName={lastName} />
+        </div>
+    );
+  }
 }
+
+const App = () => (
+  <div>
+    <SimpleForm />
+  </div>
+);
 
 export default App;
